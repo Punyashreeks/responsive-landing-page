9474cfa71bcd3d83cd9cfbe5aeb02200eed5ce2a
Punyashreeks <shreekspunya@gmail.com>
2026-07-03 07:46:58 +0530

    Add landing page UI

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Smooth welcome message
+
+console.log("Responsive Landing Page Loaded");
+
+// Button click
+
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", () => {
+
+    alert("Welcome to the Landing Page!");
+
+});
